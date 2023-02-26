909edc65e05ef60a2ccf205c80458d4ec6f67a28
Julian Hatwell <julian.hatwell@googlemail.com>
2023-02-26 15:26:07 +0800

    folder upload and sratic site

--- bucket_upload.py
+++ bucket_upload.py
@@ -10,12 +10,12 @@ import json
 arg_parser = argparse.ArgumentParser(
     prog = "BucketUpload",
     description = "Convenience CLI Application For Uploading Files to Cloud Storage",
-    epilog = "See Also BucketDownLoad -h"
+    epilog = "See Also bucket_downLoad -h"
 )
 arg_parser.add_argument("-f", "--file-name", required=True, help="File or folder to upload. Supported types: csv, json, txt")
-arg_parser.add_argument("-d", "--destination-folder", required=True)
-arg_parser.add_argument("-b", "--bucket", default=BUCKET_NAME)
-arg_parser.add_argument("-g", "--logging-bucket", default=LOGGING_BUCKET_NAME)
+arg_parser.add_argument("-b", "--bucket", default=BUCKET_NAME, help="Destination bucket")
+arg_parser.add_argument("-d", "--destination-folder", default="/")
+arg_parser.add_argument("-g", "--logging-bucket", default=LOGGING_BUCKET_NAME, help="Logging bucket")
 arg_parser.add_argument(
     "-c", "--clear-log",
     action="store_true",
@@ -24,7 +24,7 @@ arg_parser.add_argument(
 arg_parser.add_argument(
     "-j", "--to-json",
     action="store_true",
-    help="Convert to json before uploading?"
+    help="Upload object as json?"
 )
 arg_parser.add_argument(
     "--header-row",
@@ -56,13 +56,21 @@ except ValueError:
 
 lg.lp_info(f'JOB STARTING')
 path = Path(args.file_name)
-if not path.is_file() or path.is_dir():
+
+def args_d_parse_(destination_folder):
+    if destination_folder=="/":
+        return ""
+    elif destination_folder[-1]!="/":
+        destination_folder += "/"
+    return destination_folder
+
+if not (path.is_file() or path.is_dir()):
     lg.lp_error(f'INVALID FILE: -f/--file-name {args.file_name}')
     sys.exit()
 if path.is_file():
     lg.lp_info(f'Opening file {args.file_name}')
     file_stem = path.stem
-    file_destination = f'{args.destination_folder}/{file_stem}'
+    file_destination = f'{args_d_parse_(args.destination_folder)}{file_stem}'
     file_type = path.suffix.replace(".", "")
     if args.to_json:
         lg.lp_info(f'Converting {file_type} to json')
@@ -87,9 +95,28 @@ if path.is_file():
     )
     lg.lp_info(f'Uploading common log')
     common_log_("bucket_upload", f'{args.file_name} as {file_type}')
-else: # folder
-    # TODO
-    pass
+elif path.is_dir(): # folder
+    lg.lp_info(f'Scanning contents of directory {args.file_name}')
+    for p in path.rglob("*"):
+        child_source_path = str(p)
+        destination_folder = args_d_parse_(args.destination_folder)
+        child_destination_path = child_source_path.replace(f'{args.file_name}/', "")
+        child_destination_path = f'{destination_folder}{child_destination_path}'
+        source_object = {
+            "type" : "file",
+            "data" : child_source_path
+        }
+        lg.lp_info(f'Uploading {child_source_path} to {child_destination_path}')
+        upload_(
+            f'{destination_folder}{child_destination_path}',
+            source_object=source_object,
+            bucket_name=args.bucket
+        )
+    lg.lp_info(f'Uploading common log')
+    common_log_("bucket_upload", f'Uploaded all files from path walk at {args.file_name}')        
+else: # not valid?
+    lg.lp_error(f'{FileNotFoundError(args.file_name)}')
+
 
 lg.lp_info(f'JOB FINISHED')
 
